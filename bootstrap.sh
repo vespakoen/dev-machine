ff3036d031cabfa6c3b62584bedfb5becba3230a
Koen Schmeets <k.schmeets@gmail.com>
2013-10-31 06:15:43 +0100

    Installing ruby-dev to make the vagrant-salt plugin work correctly

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo apt-get install ruby1.9.1-dev
 sudo vagrant plugin install vagrant-salt
 vagrant box add precise64 http://files.vagrantup.com/precise64.box
 git clone https://github.com/Vespakoen/dev-machine.git $1
