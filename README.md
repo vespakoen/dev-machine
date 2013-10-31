29514ef7ec1e6478c2515069a67c0bd073c1d30f
Koen Schmeets <k.schmeets@gmail.com>
2013-10-31 05:25:07 +0100

    Updates

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
 My Dev Machine
-===========
+==============
 
-Optimized for 64 bit debian based systems and comes with:
+### Screenies
 
-- Ubuntu desktop (this will be removed soon)
-- GNOME shell
-- Xmonad
-- FlatStudio theme
-- Nitrux icons
+![Screenshot 1](http://github.com/Vespakoen/dev-machine/screenshots/1.png)
+
+![Screenshot 2](http://github.com/Vespakoen/dev-machine/screenshots/2.png)
+
+![Screenshot 3](http://github.com/Vespakoen/dev-machine/screenshots/3.png)
+
+Optimized for 64 bit xubuntu and comes with:
+
+- Users (sets up user accounts)
+- System (configure locale & keyboard)
+- SSH (keys, configs)
+- Git (configs)
+- Xmonad (configs)
+- FlatStudio theme (coloured by a simple setting)
+- Nitrux icons (coloured by a simple setting)
 - Virtualbox
 - Vagrant (with salty-vagrant plugin)
-- Sublime Text
-- ZSH + Oh My ZSH + Ubuntu Mono Powerline font
+- Sublime Text (with a bunch of php plugins)
+- ZSH + Oh My ZSH
+- Project manager (Clones from git to a specified location, adds "jump" bookmarks for the console, in file choosers and the file browser)
--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-#sudo apt-get install git-core curl
-#curl -L http://bootstrap.saltstack.org | sudo sh -s -- git develop
-#mkdir ~/Projects
-#cd ~/Projects && git clone git@github.com:Vespakoen/dev-machine.git
+sudo apt-get install git-core curl
+curl -L http://bootstrap.saltstack.org | sudo sh -s -- git develop
+mkdir ~/Projects
+cd ~/Projects && git clone git@github.com:Vespakoen/dev-machine.git
 sudo ln -s "/home/koen/Projects/vespakoen/code/dev-machine/roots/salt" /srv/salt
 sudo ln -s "/home/koen/Projects/vespakoen/code/dev-machine/roots/pillar" /srv/pillar
-sudo mv /etc/salt/master /etc/salt/master.old
-sudo mv /etc/salt/minion /etc/salt/minion.old
-sudo mv master /etc/salt/master
-sudo chown root:root /etc/salt/master /etc/salt/master
-sudo mv minion /etc/salt/minion
-sudo chown root:root /etc/salt/minion /etc/salt/minion
+# sudo mv /etc/salt/master /etc/salt/master.old
+# sudo mv /etc/salt/minion /etc/salt/minion.old
+# sudo mv master /etc/salt/master
+# sudo chown root:root /etc/salt/master /etc/salt/master
+# sudo mv minion /etc/salt/minion
+# sudo chown root:root /etc/salt/minion /etc/salt/minion
 sudo salt-call --local state.highstate -l debug
